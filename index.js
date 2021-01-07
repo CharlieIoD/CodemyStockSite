ce4ce11b455e82faa871c9998e86c96ed0499c76
Charles Efford <charles_a_efford@hotmail.com>
2021-01-07 19:15:38 +0000

    Major uodate 07012021

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+// Use Body Parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+// API Key: pk_5582c7f15d9443259f477f5a01241670
+// Create call_api function
+function call_api(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_5582c7f15d9443259f477f5a01241670', {json: true}, (err, res, body) => {
+if (err) {return console.log(err);}
+    if (res.statusCode === 200) {
+        // console.log(body);
+        finishedAPI(body);
+    };
+});
+}
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs());
@@ -11,11 +30,28 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "This is Otherstuff";
 
-// Set Handlebar Routes
+// Set Handlebar Index GET Route
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff:otherstuff
-    });
+    call_api(function(doneAPI) {
+        res.render('home', {
+        stock: doneAPI 
+        });
+    });   
+});
+
+// Set Handlebar Index POST Route
+app.post('/', function (req, res) {
+    call_api(function(doneAPI) {
+        // posted_stuff = req.body.stock_ticker;
+        res.render('home', {
+        stock: doneAPI
+        });
+    }, req.body.stock_ticker);   
+});
+
+// Create About page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set a static folder
